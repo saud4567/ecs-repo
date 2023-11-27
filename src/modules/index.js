2aa431d9cbe2caa60e6bdc684f1c457e49e61731
Mohd Adnan Ansari <adnan.adnan@techstalwarts.com>
2023-11-27 12:28:28 +0530

    fix: change the URLs

--- src/modules/index.js
+++ src/modules/index.js
@@ -4,15 +4,15 @@ const customer = require("../modules/customer/routes");
 const common = require("../modules/common/routes");
 
 // module_name: customer
-// module_route: /customer
+// module_route: /sso/customer
 // module_description:
 //      handles routes related to customer module
-appModules.use("/customer", customer);
+appModules.use("/sso/customer", customer);
 
 // module_name: common
 // module_route: /common
 // module_description:
 //      handles routes related to common module
-appModules.use("/common", common);
+appModules.use("/sso/common", common);
 
 module.exports = appModules;
